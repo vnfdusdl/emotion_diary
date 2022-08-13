f2c52c91b86af09dedd2153851c4007238368b5c
choyeon <rlachdus2@naver.com>
2022-08-13 15:54:18 +0900

    ✨ data state 생성 및 context 생성

--- src/App.js
+++ src/App.js
@@ -1,32 +1,92 @@
 import './App.css';
+import React, { createContext, useContext, useReducer, useRef } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Reset } from 'styled-reset';
 import Home from './pages/Home';
 import Edit from './pages/Edit';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import MyBtn from './Components/MyBtn';
 import MyHeader from './Components/MyHeader';
-import {Reset} from 'styled-reset'
+
+const reducer = (state, action) => {
+  const newState = [];
+  switch (action.type) {
+    case 'CREATE': {
+      newState = [action.data, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT': {
+      newState = state.map((it) => (it.id === action.data.id ? action.data : it));
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryDataContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+
+  const onCreat = (date, content, emotion) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+        id: dataId.current++,
+      },
+    });
+  };
+
+  const onRemove = (targetId) => {
+    dispatch({ type: 'REMOVE', targetId });
+  };
+
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: 'EDIT',
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
   return (
-    <BrowserRouter>
-    <Reset />
-      <div className='App'>
-    <MyHeader leftChild={<MyBtn text={'<'} />} headerContent={'감정 일기장'} rightChild={<MyBtn text={'>'}/>}/>
-        {/* <MyBtn text={'버튼'}  type={'psfsdfsd'} onClick={() => alert('버튼클릭!')} /> */}
-        <h1>여기는 app</h1>
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/edit' element={<Edit />}></Route>
-          <Route path='/diary' element={<Diary />}></Route>
-          <Route path='/diary/:id' element={<Diary />}></Route>
-          <Route path='/new' element={<New />}></Route>
-        </Routes>
-        {/* <img src={process.env.PUBLIC_URL +'/assets/emotion1.png'}></img> */}
-        {/* <img src={process.env.PUBLIC_URL +'/assets/emotion5.png'}></img> */}
-      </div>
-    </BrowserRouter>
+    <DiaryDataContext.Provider value={data}>
+      <DiaryDispatchContext.Provider
+        value={{
+          onCreat,
+          onRemove,
+          onEdit,
+        }}>
+        <BrowserRouter>
+          <Reset />
+          <div className='App'>
+            <h1>여기는 app</h1>
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/edit' element={<Edit />}></Route>
+              <Route path='/diary' element={<Diary />}></Route>
+              <Route path='/diary/:id' element={<Diary />}></Route>
+              <Route path='/new' element={<New />}></Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryDataContext.Provider>
   );
 }
 
