44b9dbfb79428644831da97514b484fecb26d789
choyeon <rlachdus2@naver.com>
2022-08-14 21:21:41 +0900

    ✨ emotion으로 일기 filtering 기능 추가

--- src/Components/DiaryList.js
+++ src/Components/DiaryList.js
@@ -17,12 +17,31 @@ const SortMenu = ({ value, onChange, sortOptionList }) => {
   );
 };
 
+const emotionList = [
+  { value: 'all', name: '모두' },
+  { value: 'good', name: '기분 좋은 날' },
+  { value: 'bad', name: '기분 안좋은 날' },
+];
+
+const EmotionMenu = ({ value, onChange, emotionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {emotionList.map((item, idx) => (
+        <option value={item.value} key={idx}>
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ data }) => {
   const [sortType, setSortType] = useState('latest');
+  const [emotionType, setEmotionType] = useState('all');
   const diaryList = data;
 
-
   const sortedDiaryList = () => {
+
     const compareFunc = (a, b) => {
       if (sortType === 'latest') {
         return parseInt(b.date) - parseInt(a.date);
@@ -30,16 +49,23 @@ const DiaryList = ({ data }) => {
         return parseInt(a.date) - parseInt(b.date);
       }
     };
-
+    const filterFunc = (item) => {
+        if(emotionType === 'good') {
+            return parseInt(item.emotion) <= 3
+        } else {
+            return parseInt(item.emotion) > 3
+        }
+    }
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compareFunc);
-
+    const filteredList = emotionType === 'all' ? copyList : copyList.filter((item) => filterFunc(item))
+    const sortedList = filteredList.sort(compareFunc);
     return sortedList;
   };
 
   return (
     <>
       <SortMenu value={sortType} onChange={setSortType} sortOptionList={sortOptionList} />
+      <EmotionMenu value={emotionType} onChange={setEmotionType} emotionList={emotionList} />
       {sortedDiaryList().map((it, idx) => (
         <div key={idx}>{it.content}</div>
       ))}
