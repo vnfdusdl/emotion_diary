d9dca5fccf6cf83b7c8ffcea5f52d6e2d75560f3
choyeon <rlachdus2@naver.com>
2022-08-18 00:19:18 +0900

    ⚡️ content 15자만 잘라서 보여지기

--- src/Components/DiaryItem.js
+++ src/Components/DiaryItem.js
@@ -3,24 +3,26 @@ import { useNavigate } from 'react-router-dom';
 
 const DiaryItem = ({ emotion, content, date, id }) => {
   const navigate = useNavigate();
-  
+
   const goDetail = () => {
-    navigate(`/detail/${id}`)
-  }
+    navigate(`/detail/${id}`);
+  };
   const goEdit = () => {
-    navigate(`/edit/${id}`)
-  }
+    navigate(`/edit/${id}`);
+  };
 
   const diaryDate = new Date(date).toLocaleDateString();
 
   return (
     <article className='DiaryItem'>
-      <div className={['emotion-image-wrapper', `emotion-image-wrapper-${emotion}`].join(' ')} onClick={goDetail}>
+      <div
+        className={['emotion-image-wrapper', `emotion-image-wrapper-${emotion}`].join(' ')}
+        onClick={goDetail}>
         <img src={process.env.PUBLIC_URL + `/assets/emotion${emotion}.png`} alt='기분 이미지' />
       </div>
       <div className='diary-info' onClick={goDetail}>
         <div>{diaryDate}</div>
-        <div>{content}</div>
+        <div>{content.slice(0, 15)}</div>
       </div>
       <div className='btn-wrapper' onClick={goEdit}>
         <MyBtn text={'수정하기'} />
